d923e7dbf80095146e65f0ec9c43f360d458435f
ashoke1506 <ashokedhanabal@gmail.com>
2025-07-20 19:00:50 +0530

    Initial commit: Add futuristic food tracker

---
+++ map.js
@@ -0,0 +1,10 @@
+const FOOD_DATABASE = {
+    "banana": 0.89,     // calories per gram
+    "apple": 0.52,
+    "rice": 1.3,
+    "chicken": 2.39,
+    "egg": 1.55,
+    "milk": 0.64,
+    "bread": 2.65
+  };
+  
